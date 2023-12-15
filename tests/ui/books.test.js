f2308937dda7ad98ad2557662296616fd2c81523
Yanko Rashkov <thewhitedead@abv.bg>
2023-12-15 21:34:36 +0200

    Add

--- tests/ui/books.test.js
+++ tests/ui/books.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
-
+import {pageURL,} from './pageURL.js'
 test('Check books page', async ({ page }) => {
-    await page.goto('http://localhost:8080/library');
+    await page.goto(pageURL + 'books');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
